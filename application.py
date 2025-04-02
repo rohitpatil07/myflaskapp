d9dd224edc984b96c5658f493b69a347673fbcd6
Rohit <59466377+rohitpatil07@users.noreply.github.com>
2025-04-02 10:45:42 +0530

    fix: update application.py

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-	return 'Hello World Major Changes !!!', 200      # 200 is HTTP the response code to be returned to client
+	return 'Hello World', 200      # 200 is HTTP the response code to be returned to client
 
 @app.route('/health', methods=['GET'])
 def health():
